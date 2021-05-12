5b6818a0d463c9c2e2e71fb6f0c410dc9dabda47
seagull5 <43362793+seagull5@users.noreply.github.com>
2021-05-12 22:15:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from google_trans_new import google_translator
 
 translator = google_translator()
 
-app = Flask(__name__,template_folder='templates')
+app = Flask(__name__)
 
 @app.route('/')
 def home():
