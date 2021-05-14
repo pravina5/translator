7de4c32f5cd2fb26ed261df8141beac4256e2454
seagull5 <43362793+seagull5@users.noreply.github.com>
2021-05-14 11:55:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,13 +13,14 @@ def home():
 	return render_template('apple.html')
 
 @app.route('/predict', methods=['POST'])
-'''def predict():
-	return "Hi this is your DJ host Jungkook!"   '''
 def predict():
+	message = request.form['message']
+	return message   
+'''def predict():
 	message = request.form['message']
 	text_to_translate = translator.translate(message,src ='en',dest='hi')
 	text1 = text_to_translate.text
-	return render_template('apple.html', prediction=text1)
+	return render_template('apple.html', prediction=text1)'''
 
 	
 
