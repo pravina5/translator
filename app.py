a80dc2b35ae96c474a0b5a018dae526817f8179e
seagull5 <43362793+seagull5@users.noreply.github.com>
2021-05-14 12:15:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,12 @@
 # Importing essential libraries
 from flask import Flask, render_template, request
 import requests
-from googletrans import Translator
+from google_trans_new import google_translator  
 
-translator = Translator()
+translator = google_translator() 
+#from googletrans import Translator
+
+#translator = Translator()
 
 app = Flask(__name__)
 
@@ -13,14 +16,14 @@ def home():
 	return render_template('apple.html')
 
 @app.route('/predict', methods=['POST'])
-def predict():
-	message = request.form['message']
-	return message   
 '''def predict():
 	message = request.form['message']
-	text_to_translate = translator.translate(message,src ='en',dest='hi')
-	text1 = text_to_translate.text
-	return render_template('apple.html', prediction=text1)'''
+	return message'''   
+def predict():
+	message = request.form['message']
+	text_to_translate = translator.translate(message,lang_tgt='hi')
+	#text1 = text_to_translate.text
+	return render_template('apple.html', prediction=text_to_translate)
 
 	
 
